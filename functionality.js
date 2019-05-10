90a454c8c137db61d5749b7e4d841b80b7a758e7
FontyMcPython <joseantoniofontcia@gmail.com>
2019-05-10 13:02:41 +0200

    Function to find date, sender and message fixed. The regex to split the messages is still a little primitive and should be improved. New Data[] array to hold the list of messages.

--- functionality.js
+++ functionality.js
@@ -1,55 +1,50 @@
 function showFile() {
-	var preview = document.getElementById('show-text');
-	var text = ""
-	var file = document.querySelector('input[type=file]').files[0];
-	var rdr = new FileReader(file)
+	let preview = document.getElementById('show-text');
+	let file = document.querySelector('input[type=file]').files[0];
+	let rdr = new FileReader(file);
 	rdr.onload = function (event) {
-		var content = event.target.result;
+		let content = event.target.result;
 		preview.textContent = content;
 	}
 	rdr.readAsText(file);
 }
 
+let Data = [];
 
 function analysisDo() {
-	var preview = document.getElementById('show-text');
-	var text = preview.innerHTML;
-	var comp = text.split("\n");
+	let preview = document.getElementById('show-text');
+	let text = preview.innerHTML;
+	let pattern = /\n(?=\d+\/)/;
+	let comp = text.split(pattern);
 
-	var Names = {}
-	var name
-	var i;
-	for (i=1;i<comp.length; i++) {
-		var j;
-		name = ""
-		for ( j=17; comp[i][j] != ':'; j++){
-			name = name + comp[i][j]
-		}
 
-		if (Names[name] == null) {
-			Names[name] = []
-		}
-		var data = {
-				"date": comp[i].substring(0, 8),
-				"time": comp[i].substring(9, 15),
-				"msj": comp[i].substring(j +1)
-			}
-		Names[name].push(data)
-		console.log(Names)
+
+	for (let i=0;i<comp.length-1; i++) {
+		// DATE PARSING
+		let date = comp[i].substring(0, comp[i].indexOf('-'));
+		let split1 = date.split(" ");
+		let split2 = split1[0].split("/");
+		let day = parseInt(split2[0]);
+		let month = parseInt(split2[1]);
+		let year = 2000 + parseInt(split2[2]);
+		split2 = split1[1].split(":");
+		let hour = parseInt(split2[0]);
+		let minute = parseInt(split2[1]);
+
+		// SENDER PARSING
+		let text = comp[i].substring(comp[i].indexOf('-'));
+		let sender = text.substring(2, text.indexOf(':'));
+		let msj = text.substring(text.indexOf(':')+2)
+
+		// DICTIONARY ENTRY BUILD
+		let data = {
+			"id": i+1,
+			"sender" : sender,
+			"date": new Date( year, month, day, hour, minute),
+			"msj": msj
+		};
+		Data.push(data);
 
 	}
-
-	console.log(Names)
-	//console.log(comp[1])
-	//var date = comp[1].substring(0, 8)
-	//var time = comp[1].substring(9, 15)
-	//var Name = ""
-	//var i;
-	//for ( i=17; comp[1][i] != ':'; i++){
-	//	Name = Name + comp[1][i]
-	//}
-	//console.log(date)
-	//console.log(time)
-	//console.log(Name)
-	//console.log(comp[1].substring(i +1 ))
+	console.log(Data);
 }
